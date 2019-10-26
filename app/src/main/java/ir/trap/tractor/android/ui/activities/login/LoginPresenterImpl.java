1b9a21926884696144aef20ff57ffa80f73c0d39
Abadi ,javad <javad.abadi@eniac-tech.local>
2019-10-26 18:59:30 +0330

    Merge branch 'development' of git.eniac-tech.com:Mostafa.abedi/traap-android into 10-add-transfer-money
    # Conflicts:
    #	app/src/main/java/ir/trap/tractor/android/ui/activities/main/MainActivity.java
    #	app/src/main/java/ir/trap/tractor/android/ui/fragments/simcardCharge/ChargeFragment.java
    #	app/src/main/res/layout/toolbar_back.xml
    #	app/src/main/res/layout/toolbar_main.xml

--- app/src/main/java/ir/trap/tractor/android/ui/activities/login/LoginPresenterImpl.java
+++ app/src/main/java/ir/trap/tractor/android/ui/activities/login/LoginPresenterImpl.java
@@ -142,8 +142,7 @@ public class LoginPresenterImpl implements LoginPresenter, View.OnClickListener,
 
         request.setUsername(mobileNumber.getText().toString());
         request.setCode(codeView.getText().toString());
-//        request.setCurrentVersion(BuildConfig.VERSION_NAME);0
-        request.setDevice_model(Build.BRAND + "-" + Build.MODEL);
+//        request.setCurrentVersion(BuildConfig.VERSION_NAME);
         request.setDevice_type(TrapConfig.AndroidDeviceType);
         request.setImei(IMEI_Device.getIMEI(SingletonContext.getInstance().getContext(), activityContext));
         request.setDeviceModel(Build.BRAND + "-" + Build.MODEL);
