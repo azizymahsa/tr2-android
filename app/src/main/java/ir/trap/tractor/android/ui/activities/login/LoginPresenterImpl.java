f75993491aa60bbc51ab4f2a715027b1cb347b93
reza.nejati <dev.njti@gmail.com>
2019-10-27 12:39:41 +0330

    change version

--- app/src/main/java/ir/trap/tractor/android/ui/activities/login/LoginPresenterImpl.java
+++ app/src/main/java/ir/trap/tractor/android/ui/activities/login/LoginPresenterImpl.java
@@ -143,7 +143,7 @@ public class LoginPresenterImpl implements LoginPresenter, View.OnClickListener,
         request.setUsername(mobileNumber.getText().toString());
         request.setCode(codeView.getText().toString());
 //        request.setCurrentVersion(BuildConfig.VERSION_NAME);0
-        request.setDevice_model(Build.BRAND + "-" + Build.MODEL);
+        //request.setDevice_model(Build.BRAND + "-" + Build.MODEL);
         request.setDevice_type(TrapConfig.AndroidDeviceType);
         request.setImei(IMEI_Device.getIMEI(SingletonContext.getInstance().getContext(), activityContext));
         request.setDeviceModel(Build.BRAND + "-" + Build.MODEL);
