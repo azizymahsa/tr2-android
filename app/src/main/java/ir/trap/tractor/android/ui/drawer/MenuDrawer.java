282848aca51b6e3010395a13a028bfcd58422c92
Abadi ,javad <javad.abadi@eniac-tech.local>
2019-10-28 17:45:00 +0330

    init drawer 2

--- app/src/main/java/ir/trap/tractor/android/ui/drawer/MenuDrawer.java
+++ app/src/main/java/ir/trap/tractor/android/ui/drawer/MenuDrawer.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -48,6 +49,7 @@ public class MenuDrawer extends Fragment
     private TextView user_invite_code;
     private TextView user_score;
     private TextView user_sub_state;
+    private ImageView imgMenu;
     private LinearLayout lay_user_subState;
 
     public MenuDrawer()
@@ -74,40 +76,25 @@ public class MenuDrawer extends Fragment
         // Inflating view layout
         View layout = inflater.inflate(R.layout.menudrawer_fragment, container, false);
 
-//        pref = new SharedPref(getActivity());
+        recyclerView = layout.findViewById(R.id.drawerList);
 
-        recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+        imgMenu = layout.findViewById(R.id.imgMenu);
 
-        userName = (TextView) layout.findViewById(R.id.user_name);
-        userMobile = (TextView) layout.findViewById(R.id.user_mobile);
-        user_invite_code = (TextView) layout.findViewById(R.id.user_invite_code);
-        user_score = (TextView) layout.findViewById(R.id.user_score);
-//        user_sub_state = (TextView) layout.findViewById(R.id.user_sub_state);
-//        lay_user_subState = (LinearLayout) layout.findViewById(R.id.lay_user_subState);
 
-        dataList = new ArrayList<MenuItems>();
+        dataList = new ArrayList<>();
 
-//        dataList.add(new MenuItems(1, "مشاهده لیست خرید", R.drawable.ic_logo_red));
-//
-        dataList.add(new MenuItems(1, "افزودن کارت بانکی", R.drawable.ic_logo_red));
-//        dataList.add(new MenuItems(3, "اشتراک امانات", R.drawable.img_failure));
-//        dataList.add(new MenuItems(4, "لیست امانات کتاب", R.drawable.img_failure));
-//        dataList.add(new MenuItems(5, "شارژ حساب از درگاه بانکی", R.drawable.img_failure));
-//        dataList.add(new MenuItems(6, "ویرایش اطلاعات کاربری", R.drawable.img_failure));
-        dataList.add(new MenuItems(2, "خروج از پروفایل کاربری", android.R.drawable.ic_menu_close_clear_cancel));
-//            userMobile.setVisibility(View.GONE);
-//            user_invite_code.setVisibility(View.GONE);
-//            user_score.setVisibility(View.GONE);
-//            lay_user_subState.setVisibility(View.GONE);
-//        }
-
-//        dataList.add(new MenuItems(7,"پیشنهاد به دوستان", R.string.add_person_icons));
-//        dataList.add(new MenuItems("آگه ساز", R.string.notification_bell_icons, 0));
-//        dataList.add(new MenuItems("تنظیمات نرم افزار", R.string.settings_icons));
-//        if (!pref.getToken().isEmpty())
-//        {
-//            dataList.add(new MenuItems(10, "خروج از حساب کاربری", R.string.cancel_icons));
-//        }
+        dataList.add(new MenuItems(1, "لیست تراکنش ها", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(2, "امتیازات", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(3, "جشنواره", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(4, "کیف پول", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(5, "مدیریت کارت ها", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(6, "دعوت از دوستان", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(7, "درباره ما", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(8, "تنظیمات", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(9, "پشتیبانی", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(10, "راهنما", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(11, "انتقادات و پیشنهادات", R.drawable.ic_logo_red));
+        dataList.add(new MenuItems(12, "خروج از حساب کاربری", android.R.drawable.ic_menu_close_clear_cancel));
 
         adapter = new MenuDrawerAdapter(getActivity(), dataList);
 
@@ -134,83 +121,33 @@ public class MenuDrawer extends Fragment
             }
         }));
 
-//        EventBus.getDefault().register(this);
         return layout;
     }
 
-//    @Subscribe
-//    public void userLoaded(UserProfile userProfile)
-//    {
-//        if ((userProfile.getFirst_name() ==  null) && (userProfile.getLast_name() == null))
-//        {
-//            userName.setText("UserName");
-//        }
-//        else
-//        {
-//            userName.setText(userProfile.getFullName());
-//        }
-//        if (userProfile.getMobile() != null)
-//        {
-//            userMobile.setText(userProfile.getMobile());
-//            user_invite_code.setText("کد دعوت شما: " + userProfile.getInvite_code());
-//            user_score.setText("امتیاز شما: " + userProfile.getScore());
-//
-//            if (userProfile.isMember())
-//            {
-//                user_sub_state.setText("فعال");
-//                user_sub_state.setTextColor(getResources().getColor(R.color.light_green));
-//            }
-//            else
-//            {
-//                user_sub_state.setText("غیرفعال");
-//                user_sub_state.setTextColor(getResources().getColor(R.color.red));
-//            }
-//        }
-//        else
-//        {
-//            userName.setText("کاربر مهمان");
-//            userMobile.setVisibility(View.GONE);
-//            user_invite_code.setVisibility(View.GONE);
-//            user_score.setVisibility(View.GONE);
-//            lay_user_subState.setVisibility(View.GONE);
-//
-//            dataList = new ArrayList<MenuItems>();
-//            dataList.add(new MenuItems(1, "ورود/عضویت", R.string.person_icons));
-//            dataList.add(new MenuItems(7,"پیشنهاد به دوستان", R.string.add_person_icons));
-//
-//            adapter = new MenuDrawerAdapter(getActivity(), dataList);
-//            recyclerView.setAdapter(adapter);
-//            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-//        }
-//
-//
-//    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-//        EventBus.getDefault().unregister(this);
-    }
-
-    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar)
+    {
         containerView = getActivity().findViewById(fragmentId);
         mDrawerLayout = drawerLayout;
         mDrawerToggle = new ActionBarDrawerToggle(getActivity(),
-                drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+                drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close)
+        {
             @Override
-            public void onDrawerOpened(View drawerView) {
+            public void onDrawerOpened(View drawerView)
+            {
                 super.onDrawerOpened(drawerView);
 //                getActivity().invalidateOptionsMenu();
             }
 
             @Override
-            public void onDrawerClosed(View drawerView) {
+            public void onDrawerClosed(View drawerView)
+            {
                 super.onDrawerClosed(drawerView);
 //                getActivity().invalidateOptionsMenu();
             }
 
             @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
+            public void onDrawerSlide(View drawerView, float slideOffset)
+            {
                 super.onDrawerSlide(drawerView, slideOffset);
                 toolbar.setAlpha(1 - slideOffset / 2);
             }
@@ -218,42 +155,52 @@ public class MenuDrawer extends Fragment
 
         mDrawerToggle.setDrawerIndicatorEnabled(false);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
-        mDrawerLayout.post(new Runnable() {
+        mDrawerLayout.post(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 mDrawerToggle.syncState();
             }
         });
 
     }
 
-    public static interface ClickListener {
+    public static interface ClickListener
+    {
         public void onClick(View view, int position);
 
         public void onLongClick(View view, int position);
     }
 
-    public interface FragmentDrawerListener {
+    public interface FragmentDrawerListener
+    {
         public void onDrawerItemSelected(View view, int position);
     }
 
-    static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+    static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener
+    {
 
         private GestureDetector gestureDetector;
         private ClickListener clickListener;
 
-        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener)
+        {
             this.clickListener = clickListener;
-            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener()
+            {
                 @Override
-                public boolean onSingleTapUp(MotionEvent e) {
+                public boolean onSingleTapUp(MotionEvent e)
+                {
                     return true;
                 }
 
                 @Override
-                public void onLongPress(MotionEvent e) {
+                public void onLongPress(MotionEvent e)
+                {
                     View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
-                    if (child != null && clickListener != null) {
+                    if (child != null && clickListener != null)
+                    {
                         clickListener.onLongClick(child, recyclerView.getChildPosition(child));
                     }
                 }
@@ -261,21 +208,25 @@ public class MenuDrawer extends Fragment
         }
 
         @Override
-        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e)
+        {
 
             View child = rv.findChildViewUnder(e.getX(), e.getY());
-            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e))
+            {
                 clickListener.onClick(child, rv.getChildPosition(child));
             }
             return false;
         }
 
         @Override
-        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+        public void onTouchEvent(RecyclerView rv, MotionEvent e)
+        {
         }
 
         @Override
-        public void onRequestDisallowInterceptTouchEvent(boolean b) {
+        public void onRequestDisallowInterceptTouchEvent(boolean b)
+        {
 
         }
     }
