df7eb757f069f9bdece6619578f8c5804127f7e8
mh_azizi <dev.njti@gmail.com>
2019-11-05 09:56:33 +0330

    edit profile

--- app/src/main/java/ir/trap/tractor/android/ui/activities/login/user/UserActivity.java
+++ app/src/main/java/ir/trap/tractor/android/ui/activities/login/user/UserActivity.java
@@ -29,33 +29,36 @@ import br.com.simplepass.loading_button_lib.interfaces.OnAnimationEndListener;
 import ir.trap.tractor.android.R;
 import ir.trap.tractor.android.ui.activities.main.MainActivity;
 import ir.trap.tractor.android.ui.base.GoToActivity;
+import ir.trap.tractor.android.utilities.Tools;
 import library.android.eniac.base.BaseActivity;
 
 /**
  * Created by MahtabAzizi on 10/7/2019.
  */
 public class UserActivity extends BaseActivity implements UserView, OnAnimationEndListener, View.OnClickListener
-        {
-// private UserPresenterImpl presenter;
+{
+    // private UserPresenterImpl presenter;
 //    private RelativeLayout rlImage;
-private CircularProgressButton btnConfirm;
-private EditText etFirstName, etLastName, etInvite;
-private TextView tvMenu;
-//    private ImageView ivProfile;
-private File userPic;
-private boolean isChangePic = false;
-private FrameLayout flLogoToolbar;
+    private CircularProgressButton btnConfirm;
+    private EditText etFirstName, etLastName, etInvite, etPopularPlayer;
+    private TextView tvMenu;
+    //    private ImageView ivProfile;
+    private File userPic;
+    private boolean isChangePic = false;
+    private FrameLayout flLogoToolbar;
+    private String popularPlayer;
 
 
-@Override
-protected void onCreate(Bundle savedInstanceState)
-        {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
         // presenter = new UserPresenterImpl(this, new UpdateUserImpl());
         btnConfirm = findViewById(R.id.btnConfirm);
         etFirstName = findViewById(R.id.etFirstName);
+        etPopularPlayer = findViewById(R.id.etPopularPlayer);
 //        ivProfile = findViewById(R.id.ivProfile);
         etInvite = findViewById(R.id.etInvite);
 
@@ -70,10 +73,10 @@ protected void onCreate(Bundle savedInstanceState)
         Prefs.putBoolean("isFirstLogin", true);
         setDataProfileUser();
 
-        }
+    }
 
-private void setDataProfileUser()
-        {
+    private void setDataProfileUser()
+    {
 
         etFirstName.setText(Prefs.getString("firstName", ""));
         etLastName.setText(Prefs.getString("lastName", ""));
@@ -90,80 +93,80 @@ private void setDataProfileUser()
         }
         Prefs.putString("nationalCode", profile.getNationalCode());*/
 
-        }
+    }
 
-@Override
-protected void onDestroy()
-        {
+    @Override
+    protected void onDestroy()
+    {
         super.onDestroy();
         //  presenter.onDestroy();
-        }
+    }
 
-@Override
-public void showLoading()
-        {
+    @Override
+    public void showLoading()
+    {
         btnConfirm.startAnimation();
         btnConfirm.setClickable(false);
 
-        }
+    }
 
-@Override
-public void hideLoading()
-        {
+    @Override
+    public void hideLoading()
+    {
         btnConfirm.revertAnimation(UserActivity.this);
         btnConfirm.setClickable(true);
-        }
+    }
 
-@Override
-public void onError(String message, String name, boolean b)
-        {
+    @Override
+    public void onError(String message, String name, boolean b)
+    {
         // showToast(this, message, R.color.red);
 
-        }
+    }
 
-@Override
-public void onAnimationEnd()
-        {
+    @Override
+    public void onAnimationEnd()
+    {
         btnConfirm.setText(getString(R.string.send_code));
         btnConfirm.setBackground(ContextCompat.getDrawable(this, R.drawable.background_button_login));
-        }
+    }
 
-@Override
-public void startActivity(GoToActivity activity)
-        {
+    @Override
+    public void startActivity(GoToActivity activity)
+    {
         switch (activity)
         {
-        case MainActivity:
-        startActivity(new Intent(this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-        finish();
-        break;
-        case PassCodeActivity:
-        //  startActivityForResult(new Intent(this, PassCodeActivity.class), 22);
-        break;
+            case MainActivity:
+                startActivity(new Intent(this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                finish();
+                break;
+            case PassCodeActivity:
+                //  startActivityForResult(new Intent(this, PassCodeActivity.class), 22);
+                break;
         }
-        }
+    }
 
-@Override
-public void openImageChooser()
-        {
+    @Override
+    public void openImageChooser()
+    {
         ImagePicker.create(this)
-        .returnMode(ReturnMode.ALL) // set whether pick action or camera action should return immediate result or not. Only works in single mode for image picker
-        .folderMode(true) // set folder mode (false by default)
-        .single()
-        .toolbarFolderTitle("Folder") // folder selection title
-        .toolbarImageTitle("Tap to select")
-        .toolbarDoneButtonText("DONE") // done button text
-        .start(); // image selection title
-        }
+                .returnMode(ReturnMode.ALL) // set whether pick action or camera action should return immediate result or not. Only works in single mode for image picker
+                .folderMode(true) // set folder mode (false by default)
+                .single()
+                .toolbarFolderTitle("Folder") // folder selection title
+                .toolbarImageTitle("Tap to select")
+                .toolbarDoneButtonText("DONE") // done button text
+                .start(); // image selection title
+    }
 
-@Override
-public void uploadImage()
-        {
+    @Override
+    public void uploadImage()
+    {
         if (!isChangePic)
-        return;
+            return;
         // uploadMultipart(this);
 
-        }
+    }
 
 /*    public void uploadMultipart(final Context context)
     {
@@ -298,31 +301,31 @@ public void uploadImage()
     }*/
 
 
-@Override
-protected void onActivityResult(int requestCode, int resultCode, Intent data)
-{
-    super.onActivityResult(requestCode, resultCode, data);
-    if (ImagePicker.shouldHandle(requestCode, resultCode, data))
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        // Get a list of picked images
-        Image image = ImagePicker.getFirstImageOrNull(data);
-        if (image != null)
+        super.onActivityResult(requestCode, resultCode, data);
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data))
         {
+            // Get a list of picked images
+            Image image = ImagePicker.getFirstImageOrNull(data);
+            if (image != null)
+            {
 //                ivProfile.setImageBitmap(BitmapFactory.decodeFile(image.getPath()));
-            saveImage(BitmapFactory.decodeFile(image.getPath()));
+                saveImage(BitmapFactory.decodeFile(image.getPath()));
+            }
+        }
+        if (resultCode == Activity.RESULT_OK && requestCode == 22)
+        {
+            String pass = data.getStringExtra("newPass");
+
+            //  presenter.onConfirmPassword(pass);
+
         }
     }
-    if (resultCode == Activity.RESULT_OK && requestCode == 22)
+
+    private void saveImage(Bitmap finalBitmap)
     {
-        String pass = data.getStringExtra("newPass");
-
-        //  presenter.onConfirmPassword(pass);
-
-    }
-}
-
-private void saveImage(Bitmap finalBitmap)
-        {
         isChangePic = true;
         String root = Environment.getExternalStorageDirectory().toString();
         File myDir = new File(root + "/eniac");
@@ -335,29 +338,40 @@ private void saveImage(Bitmap finalBitmap)
         if (userPic.exists()) userPic.delete();
         try
         {
-        FileOutputStream out = new FileOutputStream(userPic);
-        finalBitmap.compress(Bitmap.CompressFormat.JPEG, 30, out);
-        out.flush();
-        out.close();
+            FileOutputStream out = new FileOutputStream(userPic);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 30, out);
+            out.flush();
+            out.close();
 
         } catch (Exception e)
         {
-        e.printStackTrace();
+            e.printStackTrace();
         }
-        }
+    }
 
-@Override
-public void onClick(View view)
-        {
+    @Override
+    public void onClick(View view)
+    {
         switch (view.getId())
         {
-        case R.id.btnConfirm:
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        finish();
-        break;
+            case R.id.btnConfirm:
+
+
+                popularPlayer = etPopularPlayer.getText().toString();
+                if (popularPlayer.matches(""))
+                {
+                    Tools.showToast(this,"وارد کردن شماره بازیکن محبوب اجباری می باشد." ,R.color.red);
+
+                }else
+                {
+
+                    Intent intent = new Intent(this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+                break;
 
         }
-        }
-        }
+    }
+}
