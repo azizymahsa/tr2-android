320794be69d988f5519482af81344319a8e78b92
Abadi ,javad <javad.abadi@eniac-tech.local>
2019-11-24 17:50:02 +0330

    archive news without filter and search

---
+++ app/src/main/java/ir/traap/tractor/android/ui/activities/news/archive/NewsArchiveActivity.java
@@ -0,0 +1,18 @@
+package ir.traap.tractor.android.ui.activities.news.archive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.traap.tractor.android.R;
+
+public class NewsArchiveActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_archive);
+    }
+}
