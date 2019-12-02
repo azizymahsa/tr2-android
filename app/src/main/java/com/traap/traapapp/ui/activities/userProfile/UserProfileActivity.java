eb4618c532cbb7132e2195d60c83c0b5f4405a10
Abadi ,javad <javad.abadi@eniac-tech.local>
2019-12-02 18:42:52 +0330

    Merge branch '193-change-profile' of git.eniac-tech.com:Mostafa.abedi/traap-android into 193-change-profile
    # Conflicts:
    #	app/src/main/java/com/traap/traapapp/ui/activities/userProfile/UserProfileActivity.java

--- app/src/main/java/com/traap/traapapp/ui/activities/userProfile/UserProfileActivity.java
+++ app/src/main/java/com/traap/traapapp/ui/activities/userProfile/UserProfileActivity.java
@@ -116,7 +116,7 @@ public class UserProfileActivity extends BaseActivity implements UserProfileActi
 
         fabCapture.setOnClickListener(v ->
         {
-            openImageChooser();
+//            openImageChooser();
         });
     }
 
