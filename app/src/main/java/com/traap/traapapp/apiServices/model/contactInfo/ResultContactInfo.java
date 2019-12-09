e4d0ab1cc69d3ec0fed98a217840d6896b0a885a
azizi .Mahsa <mahsa.azizi@eniac-tech.local>
2019-12-09 17:40:36 +0330

    Merge branch 'development' of https://git.eniac-tech.com/Mostafa.abedi/traap-android into 166-add-feature-in-show-pic-activity
    # Conflicts:
    #	app/src/main/java/com/traap/traapapp/apiServices/api/RetroClient.java
    #	app/src/main/java/com/traap/traapapp/apiServices/model/bookMarkPhoto/BookMarkPhotoRequest.java
    #	app/src/main/java/com/traap/traapapp/apiServices/model/bookMarkPhoto/BookMarkPhotoResponse.java
    #	app/src/main/java/com/traap/traapapp/apiServices/part/LikeVideoService.java
    #	app/src/main/java/com/traap/traapapp/ui/activities/photo/ShowBigPhotoActivity.java

--- app/src/main/java/com/traap/traapapp/apiServices/model/contactInfo/ResultContactInfo.java
+++ app/src/main/java/com/traap/traapapp/apiServices/model/contactInfo/ResultContactInfo.java
@@ -17,6 +17,19 @@ public class ResultContactInfo
     @SerializedName("value")
     @Expose
     private String value;
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
 
     public Integer getId() {
         return id;
