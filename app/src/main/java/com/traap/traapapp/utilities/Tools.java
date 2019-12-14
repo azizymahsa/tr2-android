c529c9178296f78ac0aa0b2040f3379e5081b58b
azizi .Mahsa <mahsa.azizi@eniac-tech.local>
2019-12-14 18:17:49 +0330

    Resolve "debug app"

--- app/src/main/java/com/traap/traapapp/utilities/Tools.java
+++ app/src/main/java/com/traap/traapapp/utilities/Tools.java
@@ -43,26 +43,20 @@ public class Tools
 
     public static boolean isNetworkAvailable(Activity activity)
     {
-        ConnectivityManager connectivity = (ConnectivityManager) activity
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivity == null)
-        {
-            return false;
-        } else
-        {
-            NetworkInfo[] info = connectivity.getAllNetworkInfo();
-            if (info != null)
-            {
-                for (int i = 0; i < info.length; i++)
-                {
-                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
-                    {
-                        return true;
-                    }
-                }
-            }
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
         }
-        return false;
+        return haveConnectedWifi || haveConnectedMobile;
     }
 
     public static boolean hasICS()
