44134381a963acdc9186c8f6326bb299faa639a7
Abadi ,javad <javad.abadi@eniac-tech.local>
2019-12-17 10:21:30 +0330

    Merge branch '280-resolve-some-bug' of git.eniac-tech.com:Mostafa.abedi/traap-android into 280-resolve-some-bug
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/traap/traapapp/ui/fragments/main/MainFragment.java
    #	app/src/main/res/layout/activity_video_archive.xml
    #	app/src/main/res/layout/layout_archive_gallery.xml

--- app/src/main/java/com/traap/traapapp/apiServices/part/GetPredictService.java
+++ app/src/main/java/com/traap/traapapp/apiServices/part/GetPredictService.java
@@ -3,6 +3,7 @@ package com.traap.traapapp.apiServices.part;
 import com.traap.traapapp.apiServices.generator.ServiceGenerator;
 import com.traap.traapapp.apiServices.listener.OnServiceStatus;
 import com.traap.traapapp.apiServices.model.WebServiceClass;
+import com.traap.traapapp.apiServices.model.matchList.MatchItem;
 import com.traap.traapapp.apiServices.model.predict.getPredict.response.GetPredictResponse;
 import com.traap.traapapp.apiServices.model.stadium_rules.ResponseStadiumRules;
 
@@ -23,6 +24,11 @@ public class GetPredictService extends BasePart
     public void getPredictService(Integer matchId, OnServiceStatus<WebServiceClass<GetPredictResponse>> listener)
     {
         start(getServiceGenerator().createService().getPredict(matchId), listener);
+    }
+
+    public void getPredictEnableService(OnServiceStatus<WebServiceClass<MatchItem>> listener)
+    {
+        start(getServiceGenerator().createService().getPredictEnable(), listener);
     }
 
     public void getRulsStadium(Integer id , OnServiceStatus<WebServiceClass<ResponseStadiumRules>> listener)
