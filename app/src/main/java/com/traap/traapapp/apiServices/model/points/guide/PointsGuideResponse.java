2371250d71ce98f3bd8978c3ad46d48713c12ae4
Abadi ,javad <javad.abadi@eniac-tech.local>
2020-01-21 17:50:34 +0330

    Resolve "resolve some bug from Excel"

--- app/src/main/java/com/traap/traapapp/apiServices/model/points/guide/PointsGuideResponse.java
+++ app/src/main/java/com/traap/traapapp/apiServices/model/points/guide/PointsGuideResponse.java
@@ -1,5 +1,27 @@
 package com.traap.traapapp.apiServices.model.points.guide;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
 public class PointsGuideResponse
 {
+    @SerializedName("count")
+    @Getter @Setter
+    private int count;
+
+    @SerializedName("next")
+    @Getter @Setter
+    private String next;
+
+    @SerializedName("previous")
+    @Getter @Setter
+    private String previous;
+
+    @SerializedName("results")
+    @Getter @Setter
+    private List<PointGuide> guideList;
 }
