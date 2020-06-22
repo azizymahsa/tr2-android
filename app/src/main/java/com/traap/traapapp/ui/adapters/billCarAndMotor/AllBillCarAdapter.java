fc89ec4c9a8be728fe28b494ac2a475097e367bb
azizi .Mahsa <mahsa.azizi@eniac-tech.local>
2020-06-22 11:43:11 +0430

    Merge branch 'development' of https://git.eniac-tech.com/Mostafa.abedi/traap-android into 578-add-ui-gallery-for-introduction-player_645
    # Conflicts:
    #	app/src/main/res/layout/fragment_main.xml

--- app/src/main/java/com/traap/traapapp/ui/adapters/billCarAndMotor/AllBillCarAdapter.java
+++ app/src/main/java/com/traap/traapapp/ui/adapters/billCarAndMotor/AllBillCarAdapter.java
@@ -104,6 +104,18 @@ public class AllBillCarAdapter extends RecyclerView.Adapter<AllBillCarAdapter.Vi
             }
         });
 
+        holder.checkBox.setTag(item.getBillId());
+        holder.checkBox.setChecked(item.getCheck());
+        holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                list.get(position).setCheck(isChecked);
+
+            }
+        });
+
     }
 
     @Override
